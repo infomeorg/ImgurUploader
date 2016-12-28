b25e0c2c6acc73dd607e0f6c9af392874c0528a2
shivam kohli <kohlishivam5522@gmail.com>
2016-12-28 15:52:19 +0530

    added for three images

--- main/views.py
+++ main/views.py
@@ -19,7 +19,6 @@ from main.models import mall , codingninja , fragmentname
 
 
 def imguruploader(request):
-<<<<<<< HEAD
 	print request.FILES
 	#a = request.FILES['file1']
 	#print a.read()
@@ -73,57 +72,6 @@ def imguruploader(request):
 
 
 
-=======
-	#try:
-	print request.FILES
-	a = request.FILES['file1']
-	#print a.read()
-	client_id = 'ad3002cdda698d8'
-	encoded_string = base64.b64encode(a.read())
-	#print "the base 64 string for the corresponding image is",encoded_string
-	headers = {"Authorization": "Client-ID %s"%(client_id)}
-	print headers
-
-	url = "https://api.imgur.com/3/upload.json"
-	#file = cStringIO.StringIO(base64.b64decode(request.FILES['file1']))
-	api_key = '37ee388bf32de161bb82e3852124c0af4ae40f19'
-
-	url = "https://api.imgur.com/3/upload.json"
-
-	j1 = requests.post(
-	    url, 
-	    headers = headers,
-	    data = {
-	        'key': api_key, 
-	        'image': encoded_string,
-	        'type': 'base64',
-	        'name': '1.jpg',
-	        'title': 'Picture no. 1'
-	    }
-	)
-	print j1
-	fileurl=json.loads(j1.text)["data"]["link"]
-	print fileurl
-	q = codingninja.objects.get_or_create(pk = 1)[0]
-	q.img1 = fileurl
-	q.save()
-
-
-
-
-
-
-
-		#return HttpResponse(j1.text)
-	#except Exception as e:
-		#print e
-		#print "there was an exception"
-
-	return HttpResponse("your file was uploaded successfully")
->>>>>>> 8552c20a60d8f0ec945909a3d0e2e20f4e17e555
-
-	
-
 
 
 def index(request):
@@ -131,14 +79,6 @@ def index(request):
 
 
 
-<<<<<<< HEAD
-
-
-
-
-
-
-=======
 def apidetails(request):
 	
 	uid = request.GET.get("identifier")
@@ -180,4 +120,4 @@ def apidetails(request):
 	
 
 	return HttpResponse(response_obj)	
->>>>>>> 8552c20a60d8f0ec945909a3d0e2e20f4e17e555
+
