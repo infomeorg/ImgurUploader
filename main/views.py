db547f60a4ea349eb63fa003470e8cc996461e7f
shivam kohli <kohlishivam5522@gmail.com>
2016-12-28 15:40:53 +0530

    added for three images

--- main/views.py
+++ main/views.py
@@ -19,38 +19,59 @@ from base64 import b64encode
 
 
 def imguruploader(request):
-	#try:
-	a = request.POST
-	print a
+	print request.FILES
+	#a = request.FILES['file1']
+	#print a.read()
 	client_id = 'ad3002cdda698d8'
-
+	#encoded_string = base64.b64encode(a.read())
+	#print "the base 64 string for the corresponding image is",encoded_string
 	headers = {"Authorization": "Client-ID %s"%(client_id)}
-
-	url = "https://api.imgur.com/3/upload.json"
+	print headers
 	#file = cStringIO.StringIO(base64.b64decode(request.FILES['file1']))
 	api_key = '37ee388bf32de161bb82e3852124c0af4ae40f19'
 
-	url = "https://api.imgur.com/3/upload.json"
+	#url = "https://api.imgur.com/3/upload.json"
 
-	j1 = requests.post(
-	    url, 
-	    headers = headers,
-	    data = {
-	        'key': api_key, 
-	        'image': a,
-	        'type': 'base64',
-	        'name': '1.jpg',
-	        'title': 'Picture no. 1'
-	    }
-	)
-	print j1
-	print "naha chala bhai"
-		#return HttpResponse(j1.text)
-	#except Exception as e:
-		#print e
-		#print "there was an exception"
+	name = 'You submitted: %r' % request.POST['customer_name']
+	print name
 
-	return HttpResponse("hey there")
+	knowmore = 'You submitted: %r' % request.POST['knowmore']
+	print knowmore
+
+	aboutme = 'You submitted description is: %r' % request.POST['description']
+	print aboutme
+
+
+
+	for i in xrange(1,4):
+		url = "https://api.imgur.com/3/upload.json"
+		if ('file'+str(i)) in request.FILES:
+			image = request.FILES.get('file'+str(i), None)
+			encoded_string = base64.b64encode(image.read())
+
+			j1 = requests.post(
+			    url, 
+			    headers = headers,
+			    data = {
+			        'key': api_key, 
+			        'image': encoded_string,
+			        'type': 'base64',
+			        'name': '1.jpg',
+			        'title': 'Picture no. 1'
+			    }
+			)
+			print j1
+			fileurl=json.loads(j1.text)["data"]["link"]
+			print fileurl
+
+
+	return HttpResponse("your file was uploaded successfully")
+            
+
+
+
+
+
 
 	
 
@@ -58,3 +79,12 @@ def imguruploader(request):
 
 def index(request):
 	return render(request,'main/upload.html')
+
+
+
+
+
+
+
+
+
